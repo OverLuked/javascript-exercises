b78c76eaaa598e1e914476b10246f973fc62c440
OverLuked <edillorluke@gmail.com>
2024-12-08 21:44:59 +0800

    add answer to 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, numberOfRepeatition) {
+    let input = '';
 
+    for (let i = 0; i < numberOfRepeatition; i ++) {
+        input += string;
+    }
+
+    return input;
 };
 
 // Do not edit below this line
