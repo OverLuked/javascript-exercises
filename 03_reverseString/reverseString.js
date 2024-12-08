57aca496c924fc19bba8fde540cc066d7f7ea2c2
OverLuked <edillorluke@gmail.com>
2024-12-08 21:56:03 +0800

    add answer to 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let wordArray = string.split("");
+    let reversed = [];
 
+    for (let i = wordArray.length, j = 0; i >= 0 && j <= wordArray.length; i --, j ++) {
+        reversed[j] = wordArray[i];
+    }
+    return reversed.join('');
 };
 
 // Do not edit below this line
